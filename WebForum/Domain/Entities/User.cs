ad2ac4aad57d3933af2fc54b18789cd0b1c76916
unknown <iovanbarros@gmail.com>
2020-06-19 19:59:54 -0300

    alterando

--- WebForum/Domain/Entities/User.cs
+++ WebForum/Domain/Entities/User.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using WebForum.Domain.Validators;
 
 namespace WebForum.Domain.Entities
 {
